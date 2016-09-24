621e6a389b7241c5ca7904158d1e2a985f0ebe32
OVSS <chuangzhetianxia@gmail.com>
2016-09-24 17:36:22 +0200

    Initialize commit

--- 1-arithmetic/src/main/scala/fos/Arithmetic.scala
+++ 1-arithmetic/src/main/scala/fos/Arithmetic.scala
@@ -7,7 +7,7 @@ import scala.util.parsing.input._
  *  language of booleans and numbers found in Chapter 3 of
  *  the TAPL book.
  */
-object Arithmetic extends StandardTokenParsers {
+object ArithmeticAnother extends StandardTokenParsers {
   lexical.reserved ++= List("true", "false", "0", "if", "then", "else", "succ", "pred", "iszero")
 
   import lexical.NumericLit
@@ -20,9 +20,23 @@ object Arithmetic extends StandardTokenParsers {
              | 'pred' term
              | 'iszero' term
    */
-  def term: Parser[Term] =
-      ???
-
+  def parseNum(num:Int):Term={
+    if(num==0)
+      return Zero
+    else
+      return Succ(parseNum(num-1))
+  }
+  
+  def term: Parser[Term] = (
+        "true" ^^^ True
+      | "false" ^^^ False
+      | "if" ~ term ~ "then" ~ term ~ "else" ~ term ^^ {case _ ~cond ~ _ ~ s1 ~ "else" ~ s2 => If(cond,s1,s2)}
+      | numericLit ^^ {case num => parseNum(num.toInt)}
+      | "succ" ~ term ^^ {case _ ~ s => Succ(s)}
+      | "pred" ~ term ^^ {case _ ~ s => Pred(s)}
+      | "iszero" ~ term ^^ {case _ ~ s => IsZero(s)} 
+      )
+  
   case class NoReductionPossible(t: Term) extends Exception(t.toString)
 
   /** Return a list of at most n terms, each being one step of reduction. */
@@ -42,17 +56,64 @@ object Arithmetic extends StandardTokenParsers {
    *  If reduction is not possible NoReductionPossible exception
    *  with corresponding irreducible term should be thrown.
    */
-  def reduce(t: Term): Term =
-    ???
+  def reduce(t: Term): Term = t match{
+    case If(True,t1,t2) => t1
+    case If(False,t1,t2) => t2
+    case If(cond,t1,t2) => If(reduce(cond),t1,t2)
+    case IsZero(Zero) => True
+    case IsZero(Succ(t)) => False
+    case IsZero(t) => IsZero(reduce(t))
+    case Pred(Zero) => Zero
+    case Pred(Succ(t)) => t
+    case Pred(t) => Pred(reduce(t))
+    case Succ(t) => Succ(reduce(t))
+    case _ => throw new NoReductionPossible(t)
+  }
 
   case class TermIsStuck(t: Term) extends Exception(t.toString)
 
+  def isNumberic(t:Term):Boolean=t match{
+    case Zero => true
+    case Succ(t) => isNumberic(t)
+    case Pred(t) => isNumberic(t)
+    case _ => false
+  }
+  
+  def isValue(t:Term):Boolean=t match{
+    case True => true
+    case False => true
+    case Zero => true
+    case _ => false
+  }
+  
   /** Perform big step evaluation (result is always a value.)
    *  If evaluation is not possible TermIsStuck exception with
    *  corresponding inner irreducible term should be thrown.
    */
-  def eval(t: Term): Term =
-    ???
+  def eval(t: Term): Term = t match {
+    case v if isValue(v) => v
+    case If(cond,t1,t2) => eval(cond) match {
+      case True => eval(t1)
+      case False => eval(t2)
+      case _ => throw new TermIsStuck(t)
+    }
+    case Succ(t) => eval(t) match {
+      case num if isNumberic(t) => Succ(num)
+      case _ => throw new TermIsStuck(t)
+    }
+    case Pred(t) => eval(t) match {
+      case Zero => Zero
+      case Succ(num) if isNumberic(num) => num
+      case _ => throw new TermIsStuck(t)
+    }
+    case IsZero(t) => eval(t) match {
+      case Zero => True
+      case Succ(num) if isNumberic(num) => False
+      case _ => throw new TermIsStuck(t)
+    } 
+    case _ => throw new TermIsStuck(t)
+  }
+  
 
   def main(args: Array[String]): Unit = {
     val stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in))
